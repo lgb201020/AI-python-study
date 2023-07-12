1e6d6dd7af5d16e8145da0e402d7a1da6ec5b5e4
lgn201020 <pssdjl123@naver.com>
2023-07-12 17:02:53 +0900

    광고 클릭 예측 모델 코딩 과정 부가 설명 추가

--- AI_machinelearnning/python_ML/9-2_decision_tree_online_ad_click_prediction_model.py
+++ AI_machinelearnning/python_ML/9-2_decision_tree_online_ad_click_prediction_model.py
@@ -33,16 +33,38 @@ X_dict_train = list(train_df.drop(label_column, axis=1).T.to_dict().values())
 #target vector인 label_column(click)을 y변수로 선언
 y_train = train_df[label_column]
 
+
+
 #온라인 광고 클릭을 위한 테스트 데이터 준비
+"""
+X라는 특징 행렬을 만들기 전 가공 단계
+1. train_df에서 label column을 drop해서 데이터 준비
+2. T는 matrix transpose 기능 메서드, to_dict() 메서드는 df를 dictionary 형태로 변환 -> values만 반환
+3. 2에서의 반환받은 values를 list type으로 변환 -> list로 변환(벡터로 쓰기 위해) 
+"""
+#test_df에서 header를 0으로 선언하여 첫번째 line을 사용함을 명시적으로 표현
 test_df = pd.read_csv('C:\\Users\\기백이 노트북\\Desktop\\pythonstudy\\AI_machinelearnning\\python_ML\\train.gz', compression='gzip', header=0, sep=",", quotechar='"', skiprows=(1,100000), nrows=100000)
 test_df = test_df.drop(unused_columns, axis=1)
 X_dict_test = list(test_df.drop(label_column, axis=1).T.to_dict().values())
 y_test = test_df[label_column]
 
+
+
 #DictVectorizer 객체 생성 후 앞에서 가공한 X_dict_train과 X_dict_test를 벡터로 변환 후 X_train, X_test로 각각 선언
+"""
+원 핫 인코딩 벡터 변환
+sklearn-tree model은 입력 변수로 사용하는 모든 특징들을 수치로 입력받는다. 즉 범주형 데이터는 수치형으로 뱐환이 필요
+따라서 앞서 dictionary로 변환한 입력변수들을 list로 변환한것도 인코딩 벡터 변환을 모든 value에 일괄적으로 하기 위해서이다.
+"""
 vectorizer = DictVectorizer(sparse=True)
+#sparse=True는 벡터화 한 특징들을 희소행렬로 반환 여부에 대한 parameter이다.
+#gridsearch를 통해 최적의 parameter와 model을 찾을 때 메모리를 많이 쓰는데 특징 행렬을 그냥 행렬로 쓰면 불필요하게 0이 많아지고 그만큼 메모리 사용이 증가하기 때문에 
+#희소행렬도 불필요 0을 없애서 메모리 사용량을 최소화 
 X_train = vectorizer.fit_transform(X_dict_train)
 X_test = vectorizer.transform(X_dict_test)
+#벡터로 변환하면 19개의 column(차원)이 4952차원으로 증가함
+
+
 
 #의사 결정 트리(CART 알고리즘을 이용) 학습 모델을 선언하고 gridsearchCV로 최적의 모델 탐색
 #gridsearchCV에 사용할 hyperparams를 미리 선언
@@ -58,10 +80,15 @@ grid_search.best_params_
 #grid_search 객체로 찾은 최적의 모델 인스턴스 선언
 decision_tree_best = grid_search.best_estimator_
 
+
+
 #의사 결정 트리 모델의 파일 출력
 #decision tree graph 생성을 위한 함수 호출로 dot파일 생성
 export_graphviz(decision_tree_best, out_file="ctr_decision_tree.dot", feature_names=vectorizer.feature_names_, 
                 class_names=["0","1"], rounded=True, filled=True, impurity= True)
+
+
+
 #dot 파일을 기반으로 그래프 생성 및 파일 저장
 graph = pydot.graph_from_dot_file("ctr_decision_tree.dot")
 graph.write_png("ctr_decision_tree.png")
