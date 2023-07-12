1c35091388b18d66566e8cf4f43d210ed6bb865a
lgn201020 <pssdjl123@naver.com>
2023-07-12 18:50:23 +0900

    성능 검사 코드 추가

--- AI_machinelearnning/python_ML/9-2_decision_tree_online_ad_click_prediction_model.py
+++ AI_machinelearnning/python_ML/9-2_decision_tree_online_ad_click_prediction_model.py
@@ -17,21 +17,23 @@ print("CSV 파일이 성공적으로 저장되었습니다.")
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import export_graphviz
 import pydot
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, roc_curve
 
 
 #온라인 광고 클릭을 위한 훈련 데이터 준비
-train_df = pd.read_csv('C:\\Users\\기백이 노트북\\Desktop\\pythonstudy\\AI_machinelearnning\\python_ML\\train.gz', compression='gzip', header=0, sep=",", quotechar='"', nrows=100000)
+train_df = pd.read_csv("C:\\Users\\기백이 노트북\\Desktop\\pythonstudy\\AI_machinelearnning\\python_ML\\train.gz", compression='gzip', header=0, sep=",", quotechar='"', nrows=100000)
 unused_columns, label_column = ["id", "hour", "device_id", "device_ip"], "click"
 train_df = train_df.drop(unused_columns, axis=1)
-#target vector인 label_column(click)을 뺀 나머지 데이터를 학습 데이터로 사용
-X_dict_train = list(train_df.drop(label_column, axis=1).T.to_dict().values())
-#target vector인 label_column(click)을 y변수로 선언
-y_train = train_df[label_column]
+
+X_dict_train = list(train_df.drop(label_column, axis=1).T.to_dict().values()) #target vector인 label_column(click)을 뺀 나머지 데이터를 학습 데이터로 사용
+
+y_train = train_df[label_column] #target vector인 label_column(click)을 y변수로 선언
 
 
 
@@ -43,7 +45,7 @@ X라는 특징 행렬을 만들기 전 가공 단계
 3. 2에서의 반환받은 values를 list type으로 변환 -> list로 변환(벡터로 쓰기 위해) 
 """
 #test_df에서 header를 0으로 선언하여 첫번째 line을 사용함을 명시적으로 표현
-test_df = pd.read_csv('C:\\Users\\기백이 노트북\\Desktop\\pythonstudy\\AI_machinelearnning\\python_ML\\train.gz', compression='gzip', header=0, sep=",", quotechar='"', skiprows=(1,100000), nrows=100000)
+test_df = pd.read_csv("C:\\Users\\기백이 노트북\\Desktop\\pythonstudy\\AI_machinelearnning\\python_ML\\train.gz", compression='gzip', header=0, sep=",", quotechar='"', skiprows=(1,100000), nrows=100000)
 test_df = test_df.drop(unused_columns, axis=1)
 X_dict_test = list(test_df.drop(label_column, axis=1).T.to_dict().values())
 y_test = test_df[label_column]
@@ -67,18 +69,20 @@ X_test = vectorizer.transform(X_dict_test)
 
 
 #의사 결정 트리(CART 알고리즘을 이용) 학습 모델을 선언하고 gridsearchCV로 최적의 모델 탐색
-#gridsearchCV에 사용할 hyperparams를 미리 선언
-parameters = {'max_depth': [3, 10, None]}
-#기본 decision tree model을 생성
-decision_tree = DecisionTreeClassifier(criterion="gini",min_samples_split=30)
+
+parameters = {'max_depth': [3, 10, None]} #gridsearchCV에 사용할 hyperparams를 미리 선언
+
+decision_tree = DecisionTreeClassifier(criterion="gini",min_samples_split=30) #기본 decision tree model을 생성
+
 #gridsearch객체 선언 후 학습 ml 모델 입력, 하이퍼 파라미터 입력, scoring 방식 입력(roc_auc는 분류평가지표 중 하나로 ROC곡선 아래의 면적을 의미)
 grid_search = GridSearchCV(decision_tree, parameters, n_jobs=-1, cv=3, scoring="roc_auc")
-#선언한 gridsearch객체에 학습 데이터 적합
-grid_search.fit(X_train, y_train)
-#최적의 hyperparams를 출력
-grid_search.best_params_
-#grid_search 객체로 찾은 최적의 모델 인스턴스 선언
-decision_tree_best = grid_search.best_estimator_
+
+grid_search.fit(X_train, y_train) #선언한 gridsearch객체에 학습 데이터 적합
+
+grid_search.best_params_ #최적의 hyperparams를 출력
+
+decision_tree_best = grid_search.best_estimator_ #grid_search 객체로 찾은 최적의 모델 인스턴스 선언
+
 
 
 
@@ -92,3 +96,36 @@ export_graphviz(decision_tree_best, out_file="ctr_decision_tree.dot", feature_na
 #dot 파일을 기반으로 그래프 생성 및 파일 저장
 graph = pydot.graph_from_dot_file("ctr_decision_tree.dot")
 graph.write_png("ctr_decision_tree.png")
+
+
+
+#모델의 성능 측정
+#예측값 출력
+y_pred = decision_tree_best.predict(X_test)
+np.unique(y_pred, return_counts=True)
+
+#성능 측정
+accuracy_score(y_test, y_pred) #정확도
+
+confusion_matrix(y_test, y_pred) #confusion matrix 생성
+
+y_pred_proba = decision_tree_best.predict_proba(X_test)[:,1] #클릭할 확률과 클릭 안할 확률 중 클릭할 확률만 출력
+
+fpr, tpr = roc_curve(y_test, y_pred_proba) #roc curve X,y값 선언
+
+auc = roc_auc_score(y_test, y_pred_proba) #roc의 auc 계산
+
+
+
+#ROC curve 그래프 출력
+plt.plot(fpr, tpr, "r-", label = "Decision tree Classifier") #ROC curve 그래프 그리기
+
+plt.plot([0, 1], [0, 1], "b--", label = "random guess") #random guess 그래프 그리기
+
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("AUC{0:.2f}".format(auc))
+plt.legend(loc="lower right")
+
+plt.show()
+
